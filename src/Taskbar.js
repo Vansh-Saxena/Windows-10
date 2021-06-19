1de8ee6b964a885f098ae6db3e0e116fa8723086
vansh saxena <vsvansh030@gmail.com>
2021-06-19 14:25:55 +0530

    windows button and search button progress

--- src/Taskbar.js
+++ src/Taskbar.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import './Taskbar.css';
+import desktop from './desktop.png'
+import cortana from './cortana.png'
+import taskview from './task-view.png'
 
 class Taskbar extends Component {
     render() {
         return(
-            <h1>hello</h1>
+            <div>
+                <button><img src={desktop} alt="Hello" width="34px" height="36px"></img></button>
+                <form>
+                    <input type="search" placeholder="Type To Search" id="inp"></input>
+                </form>
+                <button><img src={cortana} alt="Hello" width="34px" height="36px"></img></button>
+                <button><img src={taskview} alt="Hello" width="34px" height="36px"></img></button>
+            </div>
         );
     }
 }
