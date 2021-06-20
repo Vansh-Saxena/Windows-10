81fae8728dfcd943c51b9f9a6e44f7c92bdad97f
vansh saxena <vsvansh030@gmail.com>
2021-06-20 10:46:58 +0530

    more changes done

--- src/Taskbar.js
+++ src/Taskbar.js
@@ -3,17 +3,28 @@ import './Taskbar.css';
 import desktop from './desktop.png'
 import cortana from './cortana.png'
 import taskview from './task-view.png'
+import taskviewhover from './Task view hover.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Taskbar extends Component {
+    
     render() {
         return(
             <div>
-                <button><img src={desktop} alt="Hello" width="34px" height="36px"></img></button>
+                <button id="windows"><img src={desktop} alt="Hello" width="34px" height="36px"></img></button>
                 <form>
-                    <input type="search" placeholder="Type To Search" id="inp"></input>
+                    <input type="search" placeholder="Type To Search" id="inp" onClick="Colorchange()"></input>
                 </form>
-                <button><img src={cortana} alt="Hello" width="34px" height="36px"></img></button>
-                <button><img src={taskview} alt="Hello" width="34px" height="36px"></img></button>
+                <button id="cortana" className="color">
+                    <a href="www.youtube.com" target="_blank">
+                    <img src={cortana} alt="Hello" width="30px" height="30px" alt="hey"></img>
+                    </a>
+                </button>
+                <button id="taskview" className="color">
+                    <a href="www.youtube.com" target="_blank">
+                    <img src={taskviewhover} alt="Hello" width="34px" height="36px" alt="hey"></img>
+                    </a>
+                </button>
             </div>
         );
     }
